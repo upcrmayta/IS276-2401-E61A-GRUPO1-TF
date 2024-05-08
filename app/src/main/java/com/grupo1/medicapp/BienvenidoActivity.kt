a05c3392d98a78a4a6d38a605c580f4cbe9b61ce
Ronald Anderson Mayta Curitumay <u201819367@upc.edu.pe>
2024-05-08 10:42:36 -0500

    Cambios en Listar Productos

--- app/src/main/java/com/grupo1/medicapp/BienvenidoActivity.kt
+++ app/src/main/java/com/grupo1/medicapp/BienvenidoActivity.kt
@@ -1,12 +1,17 @@
 package com.grupo1.medicapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class BienvenidoActivity : AppCompatActivity() {
+
+    private lateinit var btnBuscarProducto:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,15 @@ class BienvenidoActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        asignarReferencias()
+    }
+
+    fun asignarReferencias(){
+        btnBuscarProducto = findViewById(R.id.btnBuscarProducto)
+        btnBuscarProducto.setOnClickListener{
+            val intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
         }
     }
 }
