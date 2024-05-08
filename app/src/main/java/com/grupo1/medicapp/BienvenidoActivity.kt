503ade8bfa70a8663a29d8397b2c6718423da54c
Jesús López Cárdenas <jesulc96@gmail.com>
2024-05-08 16:14:47 -0500

    Modificación Usuarios

--- app/src/main/java/com/grupo1/medicapp/BienvenidoActivity.kt
+++ app/src/main/java/com/grupo1/medicapp/BienvenidoActivity.kt
@@ -1,13 +1,19 @@
 package com.grupo1.medicapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.grupo1.medicapp.entidades.Usuario
+import com.grupo1.medicapp.modelo.UsuarioDAO
 
 class BienvenidoActivity : AppCompatActivity() {
+    private lateinit var btn_Actualizar_Datos: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +28,28 @@ class BienvenidoActivity : AppCompatActivity() {
         val nombreUsuario = intent.getStringExtra("nombreUsuario")
         val textViewNombreUsuario = findViewById<TextView>(R.id.textViewNombreUsuario)
         textViewNombreUsuario.text = "Bienvenido, $nombreUsuario"
+        val cargaUsuario = nombreUsuario.toString()
+
+        loadUsuario(cargaUsuario)
     }
+
+    fun loadUsuario(datosusuario:String) {
+        btn_Actualizar_Datos = findViewById(R.id.Actualizar_Datos)
+        var datosUsuario = Usuario()
+        val usuarioDAO = UsuarioDAO(this)
+        datosUsuario = usuarioDAO.obtenerUsuario(datosusuario)
+
+        btn_Actualizar_Datos.setOnClickListener{
+            val intent = Intent (this,RegistroActivity::class.java)
+            intent.putExtra("var_id",datosUsuario.id)
+            intent.putExtra("var_usuario",datosUsuario.username)
+            intent.putExtra("var_nombres",datosUsuario.nombres)
+            intent.putExtra("var_apellido",datosUsuario.apellidos)
+            intent.putExtra("var_dni",datosUsuario.dni)
+            startActivity(intent)
+    }
+
+
+    }
+
 }
